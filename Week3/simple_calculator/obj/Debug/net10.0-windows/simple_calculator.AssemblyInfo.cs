afc8b7bd6a82eb77fa1bba373f57be93c00c0a4e
freddskii <alfred.haryanto.2006@gmail.com>
2026-09-22 10:22:23 +0700

    Adding week 3 assignment

--- Week3/simple_calculator/obj/Debug/net10.0-windows/simple_calculator.AssemblyInfo.cs
+++ Week3/simple_calculator/obj/Debug/net10.0-windows/simple_calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c066703686cc900989f40e8c4eae7b5108e782c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78d1e63dade6afe379f199554206d5c53f6ca54e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
