312f54b8196e453384530fd96aa72837d742e531
freddskii <alfred.haryanto.2006@gmail.com>
2026-09-23 07:32:42 +0700

    fixing README.md on Week3

--- Week3/simple_calculator/obj/Debug/net10.0-windows/simple_calculator.AssemblyInfo.cs
+++ Week3/simple_calculator/obj/Debug/net10.0-windows/simple_calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78d1e63dade6afe379f199554206d5c53f6ca54e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afc8b7bd6a82eb77fa1bba373f57be93c00c0a4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
